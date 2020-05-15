42751ad0dcfdc75834847fc93a0b4dcc00592c19
Jhon Gomez Marín <gmezjhon@gmail.com>
2020-05-14 20:36:26 -0500

    Desestructuracion de objetos y arreglos

---
+++ app/04_desestructuracionArreglos.ts
@@ -0,0 +1,45 @@
+/*
+    * De esta forma creamos una función
+*/
+(() => {
+    
+    /*
+        * Variable
+    */
+    const avenger = {
+        nombre: 'Steve',
+        clave: 'Capitán América',
+        poder: 'Volar'
+    }
+
+    // Desestructuracion de objetos
+    const { nombre, clave, poder } = avenger;
+    console.log( nombre, poder );
+    
+
+    //Desestructuracion objeto
+    const extraer = ( {nombre, poder}: any ) => {
+        console.log( nombre, poder );
+    }
+    extraer(avenger);
+    
+
+    //Desestructuracion arreglo
+    const avengers: string[] = ['Thor','Ironman','Spiderman'];
+    
+    //Forma simple
+    const [primero, segundo, tercero] = avengers;
+
+    console.log(primero);
+    console.log(segundo);
+    console.log(tercero);
+    
+    //forma estructurada
+    const extraerArr = ( [jhon, alfonso, gomez]: string[] ) {
+        console.log( jhon );
+        console.log( alfonso );
+        console.log( gomez );
+    }
+    extraerArr(avengers);
+
+})();
---
+++ js/04_desestructuracionArreglos.js
@@ -0,0 +1,38 @@
+"use strict";
+/*
+    * De esta forma creamos una función
+*/
+(function () {
+    /*
+        * Variable
+    */
+    var avenger = {
+        nombre: 'Steve',
+        clave: 'Capitán América',
+        poder: 'Volar'
+    };
+    // Desestructuracion de objetos
+    var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
+    console.log(nombre, poder);
+    //Desestructuracion objeto
+    var extraer = function (_a) {
+        var nombre = _a.nombre, poder = _a.poder;
+        console.log(nombre, poder);
+    };
+    extraer(avenger);
+    //Desestructuracion arreglo
+    var avengers = ['Thor', 'Ironman', 'Spiderman'];
+    //Forma simple
+    var primero = avengers[0], segundo = avengers[1], tercero = avengers[2];
+    console.log(primero);
+    console.log(segundo);
+    console.log(tercero);
+    //forma estructurada
+    var extraerArr = function (_a) {
+        var jhon = _a[0], alfonso = _a[1], gomez = _a[2];
+        console.log(jhon);
+        console.log(alfonso);
+        console.log(gomez);
+    };
+    extraerArr(avengers);
+})();
